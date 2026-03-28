c9a859a93811f8ac1d46b01791de72cf8550f90f
Bao (Danny) Ngoc Gia Nguyen <giabaovip40011@gmail.com>
2026-03-28 00:31:11 -0700

    lab10

---
+++ lab10_sp2025/server.c
@@ -0,0 +1,300 @@
+/*
+Questions to answer at top of server.c:
+(You should not need to change client.c)
+Understanding the Client:
+1. How is the client sending data to the server? What protocol?
+client.c has an array, using strncpy and send a buffer through
+TCP protocol. ie PORT, IP, SOCK_STREAM, socket(), write()
+2. What data is the client sending to the server?
+Sending each item in message[NUM_MSG] ie "Hello", "Apple", "Car", "Green", "Dog"
+Understanding the Server:
+1. Explain the argument that the `run_acceptor` thread is passed as an argument.
+The thread is passed to acceptor_args struct.
+It has stop flag, shared list_handle, and specific list lock.
+run_acceptor initialize a new non-block socket, that create MAX_CLIENTS amount
+of threads and accept MAX_CLIENTS amount of new client until the slots are
+occupied.
+2. How are received messages stored?
+They are stored as linked list data (eg list_node) with heap memory (eg malloc)
+3. What does `main()` do with the received messages?
+In particular, main() calls a main acceptor_thread, sends and joins threads from
+run_acceptor, then calls collect_all(), which traverses the linked list, prints
+messages and then free the memory. Lastly, main() returns the count sum to
+confirm if all messages were received.
+4. How are threads used in this sample code?
+acceptor_threads, an only main thread of main(), is sent to run_acceptor,
+listens and waits until enough connections and messages from non-blocking client
+sockets. Inside run_acceptor, there are MAX_CLIENTS threads that connects to
+individual client and collects data. These connections are client threads,
+allowing the serer to handle clients simultaneously.
+*/
+/*
+5)
+- How are sockets made non-blocking?
+By using fcntl(fd, F_GETFL, 0) fcntl(fd, F_SETFL, flags | O_NONBLOCK)
+- What sockets are made non-blocking?
+The Server Socket (from run_acceptor) and Client Sockets (from run_client)
+- Why are these sockets made non-blocking? What purpose does it serve?
+For Server Socket, it allows run_acceptor to check for new connections
+continuosly as long as the aargs->run is still true (only set to false in main()
+when expected amount of messages is met). If socket is blocking, server or
+run_acceptor will stop waiting for a connection at accept(). or when main
+decides to end the Server Socket, but the socket still waits for new connection
+--> Freeze server
+
+For Client Socket, it has the same reason. As soon as cargs->run is set to
+false, the loop can stop. If it's blocking, when cargs->run is false, client
+socket may still wait for read().
+*/
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define BUF_SIZE 1024
+#define PORT 8001
+#define LISTEN_BACKLOG 32
+#define MAX_CLIENTS 4
+#define NUM_MSG_PER_CLIENT 5
+
+#define handle_error(msg)                                                      \
+  do {                                                                         \
+    perror(msg);                                                               \
+    exit(EXIT_FAILURE);                                                        \
+  } while (0)
+
+struct list_node {
+  struct list_node *next;
+  void *data;
+};
+
+struct list_handle {
+  struct list_node *last;
+  volatile uint32_t count;
+};
+
+struct client_args {
+  atomic_bool run;
+
+  int cfd;
+  struct list_handle *list_handle;
+  pthread_mutex_t *list_lock;
+};
+
+struct acceptor_args {
+  atomic_bool run;
+
+  struct list_handle *list_handle;
+  pthread_mutex_t *list_lock;
+};
+
+int init_server_socket() {
+  struct sockaddr_in addr;
+
+  int sfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sfd == -1) {
+    handle_error("socket");
+  }
+
+  memset(&addr, 0, sizeof(struct sockaddr_in));
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(PORT);
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if (bind(sfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)) == -1) {
+    handle_error("bind");
+  }
+
+  if (listen(sfd, LISTEN_BACKLOG) == -1) {
+    handle_error("listen");
+  }
+
+  return sfd;
+}
+
+// Set a file descriptor to non-blocking mode
+void set_non_blocking(int fd) {
+  int flags = fcntl(fd, F_GETFL, 0);
+  if (flags == -1) {
+    perror("fcntl F_GETFL");
+    exit(EXIT_FAILURE);
+  }
+  if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+    perror("fcntl F_SETFL");
+    exit(EXIT_FAILURE);
+  }
+}
+
+void add_to_list(struct list_handle *list_handle, struct list_node *new_node) {
+  struct list_node *last_node = list_handle->last;
+  last_node->next = new_node;
+  list_handle->last = last_node->next;
+  list_handle->count++;
+}
+
+int collect_all(struct list_node head) {
+  struct list_node *node = head.next; // get first node after head
+  uint32_t total = 0;
+
+  while (node != NULL) {
+    printf("Collected: %s\n", (char *)node->data);
+    total++;
+
+    // Free node and advance to next item
+    struct list_node *next = node->next;
+    free(node->data);
+    free(node);
+    node = next;
+  }
+
+  return total;
+}
+
+static void *run_client(void *args) {
+  struct client_args *cargs = (struct client_args *)args;
+  int cfd = cargs->cfd;
+  set_non_blocking(cfd);
+
+  char msg_buf[BUF_SIZE];
+
+  while (cargs->run) {
+    ssize_t bytes_read = read(cfd, &msg_buf, BUF_SIZE);
+    if (bytes_read == -1) {
+      if (!(errno == EAGAIN || errno == EWOULDBLOCK)) {
+        perror("Problem reading from socket!\n");
+        break;
+      }
+    } else if (bytes_read > 0) {
+      // Create node with data
+      struct list_node *new_node = malloc(sizeof(struct list_node));
+      new_node->next = NULL;
+      new_node->data = malloc(BUF_SIZE);
+      memcpy(new_node->data, msg_buf, BUF_SIZE);
+
+      struct list_handle *list_handle = cargs->list_handle;
+      // TODO: Safely use add_to_list to add new_node to the list
+      pthread_mutex_lock(cargs->list_lock);
+      add_to_list(list_handle, new_node);
+      pthread_mutex_unlock(cargs->list_lock);
+    }
+  }
+
+  if (close(cfd) == -1) {
+    perror("client thread close");
+  }
+  return NULL;
+}
+
+static void *run_acceptor(void *args) {
+  int sfd = init_server_socket();
+  set_non_blocking(sfd);
+
+  struct acceptor_args *aargs = (struct acceptor_args *)args;
+  pthread_t threads[MAX_CLIENTS];
+  struct client_args client_args[MAX_CLIENTS];
+
+  printf("Accepting clients...\n");
+
+  uint16_t num_clients = 0;
+  while (aargs->run) {
+    if (num_clients < MAX_CLIENTS) {
+      int cfd = accept(sfd, NULL, NULL);
+      if (cfd == -1) {
+        if (!(errno == EAGAIN || errno == EWOULDBLOCK)) {
+          handle_error("accept");
+        }
+      } else {
+        printf("Client connected!\n");
+
+        client_args[num_clients].cfd = cfd;
+        client_args[num_clients].run = true;
+        client_args[num_clients].list_handle = aargs->list_handle;
+        client_args[num_clients].list_lock = aargs->list_lock;
+
+        // TODO: Create a new thread to handle the client
+        if (pthread_create(&threads[num_clients], NULL, run_client,
+                           &client_args[num_clients]) != 0) {
+          perror("pthread_create");
+          close(cfd);
+          continue;
+        }
+        num_clients++;
+      }
+    }
+  }
+
+  printf("Not accepting any more clients!\n");
+
+  // Shutdown and cleanup
+  for (int i = 0; i < num_clients; i++) {
+    // TODO: Set flag to stop the client thread
+    client_args[i].run = false;
+    // TODO: Wait for the client thread and close its socket
+    pthread_join(threads[i], NULL);
+    close(client_args[i].cfd);
+  }
+
+  if (close(sfd) == -1) {
+    perror("closing server socket");
+  }
+  return NULL;
+}
+
+int main() {
+  pthread_mutex_t list_mutex;
+  pthread_mutex_init(&list_mutex, NULL);
+
+  // List to store received messages
+  // - Do not free list head (not dynamically allocated)
+  struct list_node head = {NULL, NULL};
+  struct list_node *last = &head;
+  struct list_handle list_handle = {
+      .last = &head,
+      .count = 0,
+  };
+
+  pthread_t acceptor_thread;
+  struct acceptor_args aargs = {
+      .run = true,
+      .list_handle = &list_handle,
+      .list_lock = &list_mutex,
+  };
+  pthread_create(&acceptor_thread, NULL, run_acceptor, &aargs);
+
+  // TODO: Wait until enough messages are received
+  while (true) {
+    pthread_mutex_lock(&list_mutex);
+    if (list_handle.count >= MAX_CLIENTS * NUM_MSG_PER_CLIENT) {
+      pthread_mutex_unlock(&list_mutex);
+      aargs.run = false;
+      break;
+    }
+    pthread_mutex_unlock(&list_mutex);
+  }
+  pthread_join(acceptor_thread, NULL);
+
+  if (list_handle.count != MAX_CLIENTS * NUM_MSG_PER_CLIENT) {
+    printf("Not enough messages were received!\n");
+    return 1;
+  }
+
+  int collected = collect_all(head);
+  printf("Collected: %d\n", collected);
+  if (collected != list_handle.count) {
+    printf("Not all messages were collected!\n");
+    return 1;
+  } else {
+    printf("All messages were collected!\n");
+  }
+
+  pthread_mutex_destroy(&list_mutex);
+
+  return 0;
+}
